5948082fec7351f34853842f1cd9b807f48b782c
“Rachiey” <rachaelyonoff@gmail.com>
2021-10-18 14:44:21 +0100

    added setup.sql

---
+++ server/database/1_setup.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS neighbourhood;
+
+CREATE TABLE neighbourhood (
+    id serial PRIMARY KEY,
+    name varchar(25) NOT NULL,
+    age int NOT NULL CHECK(age > 0),
+    household_number int NOT NULL varchar(25),
+);
+
+DROP TABLE IF EXISTS houses;
+
+CREATE TABLE houses (
+    id serial PRIMARY KEY,
+    address varchar(50) NOT NULL,
+    owner varchar(255),
+);
+
+DROP TABLE IF EXISTS addresses;
+
+CREATE TABLE addresses (
+    id serial PRIMARY KEY,
+    postcode varchar(10) NOT NULL,
+    street varchar(255) NOT NULL,
+);
