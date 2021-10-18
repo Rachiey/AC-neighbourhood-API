5a800e01a49a29c2b443c5751c8cbdd818529100
“Rachiey” <rachaelyonoff@gmail.com>
2021-10-18 15:01:06 +0100

    added seed.sql file containing neighbourhood, houses and address

--- server/database/1_setup.sql
+++ server/database/1_setup.sql
@@ -4,7 +4,7 @@ CREATE TABLE neighbourhood (
     id serial PRIMARY KEY,
     name varchar(25) NOT NULL,
     age int NOT NULL CHECK(age > 0),
-    household_number int NOT NULL varchar(25),
+    household_number int NOT NULL varchar(25)
 );
 
 DROP TABLE IF EXISTS houses;
@@ -12,13 +12,13 @@ DROP TABLE IF EXISTS houses;
 CREATE TABLE houses (
     id serial PRIMARY KEY,
     address varchar(50) NOT NULL,
-    owner varchar(255),
+    owner_id int
 );
 
 DROP TABLE IF EXISTS addresses;
 
-CREATE TABLE addresses (
+CREATE TABLE address (
     id serial PRIMARY KEY,
     postcode varchar(10) NOT NULL,
-    street varchar(255) NOT NULL,
+    street varchar(255) NOT NULL
 );
---
+++ server/database/2_seed.sql
@@ -0,0 +1,26 @@
+INSERT INTO neighbourhood (name, age, household_number)
+VALUES
+('Benjamin','24','1'),
+('Boone','22','1'),
+('Hugh','32','2'),
+('Maple','21','2'),
+('Wolfgang','28','1'),
+('Victoria','37','1');
+
+INSERT INTO houses (address, owner_id)
+VALUES
+('22 Blossom Road','1'),
+('47 Orchid Way','2'),
+('12 Cherry Drive','3'),
+('14 Cherry Drive','4'),
+('31 Daisy Grove','5'),
+('32 Blossom Road','6');
+
+INSERT INTO address (postcode, street)
+VALUES
+('AC21 BR2','22 Blossom Road'),
+('AC21 OW4','47 Orchid Way'),
+('AC21 CD2','12 Cherry Drive'),
+('AC21 CD4','14 Cherry Drive'),
+('AC21 DG3','31 Daisy Grove'),
+('AC21 BR3','32 Blossom Road');
