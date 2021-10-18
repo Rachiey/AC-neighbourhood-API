b263349ea14bbfdda4498c65b2e343e29ea083d9
“Rachiey” <rachaelyonoff@gmail.com>
2021-10-18 15:25:06 +0100

    added server.js including routes locations

---
+++ server/api/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+const neighbourhoodRoutes = require('./controllers/neighbourhood')
+const housesRoutes = require('./controllers/houses')
+const addressRoutes = require('./controllers/address')
+
+server.use('/neighbourhood', neighbourhoodRoutes)
+server.use('/houses', housesRoutes)
+server.use('/address', addressRoutes)
+
+const port = process.env.PORT || 3000;
+
+// Root route
+server.get('/', (req, res) => res.send('Welcome to the the Neighbourhood API. Please use /neighbourhood to discover our neighbourhood.'))
+
+module.exports = server
